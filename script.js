4d36de64fee9ec0c4c1b5490a89622b32f7ad5c7
darkfarbe <ximena.ruiz.co@gmail.com>
2022-12-30 12:29:09 +0100

    showing real data
    Showing current temperature, wind speed and humidity

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
 
+  let cityElement = document.querySelector("#city");
+
+  let descriptionElement = document.querySelector("#description");
+
+  let humidityElement = document.querySelector("#humidity");
+
+  let windElement = document.querySelector("#wind");
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  console.log(response.data.wind.speed);
+
+  cityElement.innerHTML = response.data.city;
+
+  descriptionElement.innerHTML = response.data.condition.description;
+
+  humidityElement.innerHTML = Math.round(response.data.temperature.humidity);
+
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "fbde5cao1a5748d107tcc6736273f093";
+
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
